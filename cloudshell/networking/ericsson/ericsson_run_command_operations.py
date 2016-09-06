10dd5447ab8e2cc753e36ed9e1961a4caf6baf41
CoYe <wiseman.kostya@gmail.com>
2016-09-06 23:19:15 +0300

    updated ericsson_send_command_operations

--- cloudshell/networking/ericsson/ericsson_run_command_operations.py
+++ cloudshell/networking/ericsson/ericsson_run_command_operations.py
@@ -2,17 +2,16 @@ from cloudshell.configuration.cloudshell_cli_binding_keys import CLI_SERVICE
 from cloudshell.configuration.cloudshell_shell_core_binding_keys import LOGGER, API
 import inject
 
-from cloudshell.networking.operations.interfaces.send_command_interface import SendCommandInterface
+from cloudshell.networking.operations.interfaces.run_command_interface import RunCommandInterface
 from cloudshell.shell.core.context_utils import get_resource_name
 
 
-class EricssonSendCommandOperations(SendCommandInterface):
+class EricssonRunCommandOperations(RunCommandInterface):
     def __init__(self, resource_name=None, cli=None, logger=None, api=None):
-        """Create CiscoIOSHandlerBase
+        """Create EricssonRunCommandOperations
 
         :param cli: CliService object
         :param logger: QsLogger object
-        :param snmp: QualiSnmp object
         :param api: CloudShell Api object
         :param resource_name: resource name
         :return:
@@ -25,7 +24,7 @@ class EricssonSendCommandOperations(SendCommandInterface):
         try:
             self.resource_name = resource_name or get_resource_name()
         except Exception:
-            raise Exception('CiscoHandlerBase', 'Failed to get resource_name.')
+            raise Exception('EricssonRunCommandOperations', 'Failed to get resource_name.')
 
     @property
     def logger(self):
@@ -49,8 +48,8 @@ class EricssonSendCommandOperations(SendCommandInterface):
             self._cli = inject.instance(CLI_SERVICE)
         return self._cli
 
-    def send_command(self, command, expected_str=None, expected_map=None, timeout=None, retries=None,
-                     is_need_default_prompt=True, session=None):
+    def run_custom_command(self, command, expected_str=None, expected_map=None, timeout=None, retries=None,
+                           is_need_default_prompt=True, session=None):
         """Send command using cli service
 
         :param command: command to send
@@ -75,8 +74,8 @@ class EricssonSendCommandOperations(SendCommandInterface):
                                              is_need_default_prompt=is_need_default_prompt)
         return response
 
-    def send_config_command(self, command, expected_str=None, expected_map=None, timeout=None, retries=None,
-                            is_need_default_prompt=True):
+    def run_custom_config_command(self, command, expected_str=None, expected_map=None, timeout=None, retries=None,
+                                  is_need_default_prompt=True):
         """Send list of config commands to the session
 
         :param command: list of commands to send
@@ -85,8 +84,6 @@ class EricssonSendCommandOperations(SendCommandInterface):
         :rtype: string
         """
 
-        return self.cli.send_config_command(command, expected_str, expected_map, timeout, retries,
-                                            is_need_default_prompt)
-
-
-
+        return self.cli.send_config_command(command=command, expected_str=expected_str,
+                                            expected_map=expected_map, timeout=timeout,
+                                            retries=retries, is_need_default_prompt=is_need_default_prompt)
